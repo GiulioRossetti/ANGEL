fdb5ec19c8e998e936f134123e491db590c6afd1
GiulioRossetti <giulio.rossetti@gmail.com>
2023-06-07 15:41:32 +0200

    :bug: fix

--- angel/test/angel_test.py
+++ angel/test/angel_test.py
@@ -10,7 +10,7 @@ class DemonTestCase(unittest.TestCase):
 
     def test_angel(self):
         g = nx.karate_club_graph()
-        nx.write_edgelist(g, "test.csv", delimiter=" ")
+        nx.write_edgelist(g, "test.csv", delimiter=" ", data=False)
 
         an = a.Angel("test.csv", threshold=0.6, min_comsize=3, save=True)
         coms = an.execute()
